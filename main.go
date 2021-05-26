bb79eda88d339dc9ccb2c3822442a523f43e72d7
Yaeger Moon <60789654+YaegerMoon@users.noreply.github.com>
2021-05-27 01:32:37 +0900

    Make Controller of Deepzoom (#6)
    * Make Base Structure of Contorller
    
    * URL Param Validation Check
    
    * Add DeepZoom Service

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/YaegerMoon/deepzoom/controller"
+	"github.com/YaegerMoon/deepzoom/services"
+	"github.com/gin-gonic/gin"
+)
+
+const API_PREFIX = "/api/v1"
 
 func main() {
-	fmt.Println("Dev")
+
+	router := gin.Default()
+	regionDeepZoom := services.New(33, 1, 1, "png")
+	controller.New(API_PREFIX, router, regionDeepZoom)
+	router.Run(":8080")
 }
