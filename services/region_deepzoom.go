c6da7c2da03daf12c9bc7e064d70df3002f9b6a6
YaegerMoon <yaeger@kakao.com>
2021-06-07 23:56:35 +0900

    New function of DeepZoom LevelTileDimension

--- services/region_deepzoom.go
+++ services/region_deepzoom.go
@@ -1,15 +1,17 @@
 package services
 
-import "github.com/jammy-dodgers/gophenslide/openslide"
+import (
+	"github.com/jammy-dodgers/gophenslide/openslide"
+)
 
 type RegionDeepZoom struct {
-	TileSize   int
+	TileSize   int64
 	Overlap    int
 	LimitBound int
 	Format     string
 }
 
-func New(tileSize int, overlap int, limitbound int, format string) *RegionDeepZoom {
+func New(tileSize int64, overlap int, limitbound int, format string) *RegionDeepZoom {
 	return &RegionDeepZoom{tileSize, overlap, limitbound, format}
 }
 
@@ -18,7 +20,17 @@ func (dz *RegionDeepZoom) LevelCount(path string) int32 {
 	if err != nil {
 		panic("Fail to open slide image")
 	}
-
 	levelCount := openslide.LevelCount()
 	return levelCount
+}
+
+func (dz *RegionDeepZoom) LevelTileDimension(path string, level int32) (int64, int64) {
+	openslide, err := openslide.Open(path)
+	if err != nil {
+		panic("Fail to open slide image")
+	}
+	w, h := openslide.LevelDimensions(level)
+	tileW := w / dz.TileSize
+	tileH := h / dz.TileSize
+	return tileH, tileW
 }
