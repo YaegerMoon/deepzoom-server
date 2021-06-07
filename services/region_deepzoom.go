7836c0d2b11106365a1ad21915f35e292d38eaea
YaegerMoon <yaeger@kakao.com>
2021-06-07 23:20:52 +0900

    Function of RegionDeepZoom (LevelCount)

--- services/region_deepzoom.go
+++ services/region_deepzoom.go
@@ -1,5 +1,7 @@
 package services
 
+import "github.com/jammy-dodgers/gophenslide/openslide"
+
 type RegionDeepZoom struct {
 	TileSize   int
 	Overlap    int
@@ -9,4 +11,14 @@ type RegionDeepZoom struct {
 
 func New(tileSize int, overlap int, limitbound int, format string) *RegionDeepZoom {
 	return &RegionDeepZoom{tileSize, overlap, limitbound, format}
+}
+
+func (dz *RegionDeepZoom) LevelCount(path string) int32 {
+	openslide, err := openslide.Open(path)
+	if err != nil {
+		panic("Fail to open slide image")
+	}
+
+	levelCount := openslide.LevelCount()
+	return levelCount
 }
